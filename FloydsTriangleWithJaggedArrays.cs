76d40b7d2091b748e5646c3db8af1363df81dc0b
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-02-21 01:27:01 +0530

    Update FloydsTriangleWithJaggedArrays.cs

--- FloydsTriangleWithJaggedArrays.cs
+++ FloydsTriangleWithJaggedArrays.cs
@@ -7,13 +7,8 @@ class Program
         Console.Write("Enter the number of rows: ");
         int rows = int.Parse(Console.ReadLine());
 
-        int num = 1;
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j <= i; j++)
-                Console.Write($"{num++,3} ");
-            Console.WriteLine();
-        }
+        for (int i = 1, num = 1; i <= rows; Console.WriteLine(), i++)
+            for (int j = 0; j < i; Console.Write($"{num++,3} "), j++) ;
 
         Console.ReadKey();
     }

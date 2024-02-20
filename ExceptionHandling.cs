6e09d3e7c3834175053f252a4a5adedd5bed2f1a
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-02-21 01:18:49 +0530

    Update ExceptionHandling.cs

--- ExceptionHandling.cs
+++ ExceptionHandling.cs
@@ -6,7 +6,6 @@ class Program
     {
         int[] arr = { 1, 2, 3, 4, 5 };
 
-        // Print array elements
         for (int i = 0; i < arr.Length; i++)
         {
             Console.WriteLine(arr[i]);
@@ -14,15 +13,12 @@ class Program
 
         try
         {
-            // Access an out-of-bounds index (7th element)
             Console.WriteLine(arr[7]);
         }
         catch (IndexOutOfRangeException e)
         {
             Console.WriteLine($"An Exception has occurred: {e.Message}");
         }
-
-        // Wait for user input before exiting
         Console.ReadKey();
     }
 }

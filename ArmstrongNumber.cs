43c893c87f0a96723bd285413a2a41aaa22cbec6
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-02-21 00:59:58 +0530

    Update ArmstrongNumber.cs

--- ArmstrongNumber.cs
+++ ArmstrongNumber.cs
@@ -6,19 +6,18 @@ class Program
     {
         Console.WriteLine("Armstrong Numbers (1-1000):");
         for (int n = 1; n <= 1000; n++)
-            if (IsArmstrong(n)) Console.WriteLine(n);
-        Console.ReadKey();
+            if (IsArmstrong(n))
+                Console.WriteLine(n);
     }
 
     static bool IsArmstrong(int num)
     {
-        int sum = 0, temp = num;
-        while (num > 0)
+        int sum = 0;
+        for (int temp = num; temp > 0; temp /= 10)
         {
-            int digit = num % 10;
+            int digit = temp % 10;
             sum += digit * digit * digit;
-            num /= 10;
         }
-        return sum == temp;
+        return sum == num;
     }
 }
